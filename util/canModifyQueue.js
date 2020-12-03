66de8d80e84ae0584a14693f2524e7def00d7248
rtgamingwdt <46906497+rtgamingwdt@users.noreply.github.com>
2020-12-03 15:04:39 -0500

    Create canModifyQueue.js

---
+++ util/canModifyQueue.js
@@ -0,0 +1,11 @@
+module.exports.canModifyQueue = async (member) => {
+  const { channelID } = member.voice;
+  const botChannel = member.guild.voice.channelID;
+
+  if (channelID !== botChannel) {
+    member.send("You need to join the voice channel first!").catch(console.error);
+    return;
+  }
+
+  return true;
+};
