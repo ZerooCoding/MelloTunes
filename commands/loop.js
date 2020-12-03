9b4727384210c8ad4a17c5f3bea4d88f49c4dc55
rtgamingwdt <46906497+rtgamingwdt@users.noreply.github.com>
2020-12-03 15:38:17 -0500

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -4,19 +4,17 @@ const {
 const sendError = require("../util/error");
 
 module.exports.run = async (client, message, args) => {
-    let LOOP = client.emojis.cache.find(emoji => emoji.id === '778876617744318484')
-    let WARNING = client.emojis.cache.find(emoji => emoji.id === '778868936874655774')
         const serverQueue = message.client.queue.get(message.guild.id);
         if (serverQueue) {
             serverQueue.loop = !serverQueue.loop;
             return message.channel.send({
                 embed: {
                     color: "GREEN",
-                    description: `${LOOP}  **|**  Loop is **\`${serverQueue.loop === true ? "enabled" : "disabled"}\`**`
+                    description: `  **|**  Loop is **\`${serverQueue.loop === true ? "enabled" : "disabled"}\`**`
                 }
             });
         };
-        return sendError(`${WARNING}There is nothing playing in this server.`, message.channel);
+        return sendError(`There is nothing playing in this server.`, message.channel);
     }
 
 	module.exports.config = {
