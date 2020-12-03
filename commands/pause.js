61694527d395fcbd5ca18df0a211f7416de5bda0
rtgamingwdt <46906497+rtgamingwdt@users.noreply.github.com>
2020-12-03 15:52:22 -0500

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -4,9 +4,6 @@ const {
 const sendError = require("../util/error");
 
 module.exports.run = async (client, message, args) => {
-    let WARNING = client.emojis.cache.find(emoji => emoji.id === '778868936874655774')
-    let NOTES = client.emojis.cache.find(emoji => emoji.id === '778877611921244160')
-    let PAUSE = client.emojis.cache.find(emoji => emoji.id === '778875419612151808')
         const serverQueue = message.client.queue.get(message.guild.id);
         if (serverQueue && serverQueue.playing) {
             serverQueue.playing = false;
@@ -14,15 +11,15 @@ module.exports.run = async (client, message, args) => {
                 serverQueue.connection.dispatcher.pause()
             } catch (error) {
                 message.client.queue.delete(message.guild.id);
-                return sendError(`${NOTES} The player has stopped and the queue has been cleared.: ${error}`, message.channel);
+                return sendError(`The player has stopped and the queue has been cleared.: ${error}`, message.channel);
             }
             let xd = new MessageEmbed()
-                .setDescription(`${PAUSE} Paused the music for you!`)
+                .setDescription(`Paused the music for you!`)
                 .setColor("YELLOW")
                 .setTitle("Music has been paused!")
             return message.channel.send(xd);
         }
-        return sendError(`${WARNING}There is nothing playing in this server.`, message.channel);
+        return sendError(`There is nothing playing in this server.`, message.channel);
     }
 
 	module.exports.config = {
