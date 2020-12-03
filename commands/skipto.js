a0e35499e3aa8f0d7e0790fb7680c09f3f56590f
rtgamingwdt <46906497+rtgamingwdt@users.noreply.github.com>
2020-12-03 16:24:37 -0500

    Update skipto.js

--- commands/skipto.js
+++ commands/skipto.js
@@ -4,12 +4,6 @@ const {
 const sendError = require("../util/error");
 
 module.exports.run = async (client, message, args) => {
-    	let WARNING = client.emojis.cache.find(emoji => emoji.id === '778868936874655774')
-    	let CHECK_MARK = client.emojis.cache.find(emoji => emoji.id === '778889804426248194')
-        let STOP = client.emojis.cache.find(emoji => emoji.id === '780290077237968907')
-        let PLAY = client.emojis.cache.find(emoji => emoji.id === '778881750029434905')
-        let NOTES = client.emojis.cache.find(emoji => emoji.id === '778877611921244160')
-        let FF = client.emojis.cache.find(emoji => emoji.id === '778878999819845672')
         if (!args.length || isNaN(args[0]))
             return message.channel.send({
                 embed: {
@@ -21,9 +15,9 @@ module.exports.run = async (client, message, args) => {
 
 
         const queue = message.client.queue.get(message.guild.id);
-        if (!queue) return sendError(`${WARNING}There is no queue.`, message.channel).catch(console.error);
+        if (!queue) return sendError(`There is no queue.`, message.channel).catch(console.error);
         if (args[0] > queue.songs.length)
-            return sendError(`${WARNING}The queue is only ${queue.songs.length} songs long!`, message.channel).catch(console.error);
+            return sendError(`The queue is only ${queue.songs.length} songs long!`, message.channel).catch(console.error);
 
         queue.playing = true;
 
@@ -39,17 +33,17 @@ module.exports.run = async (client, message, args) => {
         } catch (error) {
             queue.voiceChannel.leave()
             message.client.queue.delete(message.guild.id);
-            return sendError(`${NOTES} The player has stopped and the queue has been cleared.: ${error}`, message.channel);
+            return sendError(`The player has stopped and the queue has been cleared.: ${error}`, message.channel);
         }
 
         queue.textChannel.send({
             embed: {
                 color: "GREEN",
-                description: `${message.author} ${FF} skipped \`${args[0] - 1}\` songs`
+                description: `${message.author} skipped \`${args[0] - 1}\` songs`
             }
 
         }).catch(console.error);
-        message.react("783079533300809778")
+        message.react("👌")
     }
 
 	module.exports.config = {
