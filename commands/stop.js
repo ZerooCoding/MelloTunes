0e09913a2ce1908decc836afd0f5267a76b6f5bb
rtgamingwdt <46906497+rtgamingwdt@users.noreply.github.com>
2020-12-03 16:25:43 -0500

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -4,15 +4,10 @@ const {
 const sendError = require("../util/error");
 
 module.exports.run = async (client, message, args) => {
-    	let WARNING = client.emojis.cache.find(emoji => emoji.id === '778868936874655774')
-    	let CHECK_MARK = client.emojis.cache.find(emoji => emoji.id === '778889804426248194')
-        let STOP = client.emojis.cache.find(emoji => emoji.id === '780290077237968907')
-        let PLAY = client.emojis.cache.find(emoji => emoji.id === '778881750029434905')
-        let NOTES = client.emojis.cache.find(emoji => emoji.id === '778877611921244160')
         const channel = message.member.voice.channel
-        if (!channel) return sendError(`${WARNING}You need to be in a voice channel to play music!`, message.channel);
+        if (!channel) return sendError(`You need to be in a voice channel to play music!`, message.channel);
         const serverQueue = message.client.queue.get(message.guild.id);
-        if (!serverQueue) return sendError(`${WARNING}There is nothing playing that I could stop for you.`, message.channel);
+        if (!serverQueue) return sendError(`There is nothing playing that I could stop for you.`, message.channel);
         if (!serverQueue.connection) return
         if (!serverQueue.connection.dispatcher) return
         try {
@@ -20,11 +15,11 @@ module.exports.run = async (client, message, args) => {
         } catch (error) {
             message.guild.me.voice.channel.leave();
             message.client.queue.delete(message.guild.id);
-            return sendError(`${NOTES} The player has stopped and the queue has been cleared.: ${error}`, message.channel);
+            return sendError(`The player has stopped and the queue has been cleared.: ${error}`, message.channel);
         }
         message.client.queue.delete(message.guild.id);
         serverQueue.songs = [];
-        message.react("778889804426248194")
+        message.react("👌")
     }
 
 	module.exports.config = {
