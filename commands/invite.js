20d1515b8702d2cb04e7bf177ab9e4f70979c14d
rtgamingwdt <46906497+rtgamingwdt@users.noreply.github.com>
2020-12-03 15:36:41 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -10,7 +10,6 @@ module.exports.run = async (client, message, args) => {
         let invite = new MessageEmbed()
             .setTitle(`Invite ${client.user.username}`)
             .setDescription(`Want me in your server? Invite me today! \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot)`)
-            .setURL(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot`)
             .setColor("BLUE")
         return message.channel.send(invite);
     }
