bc10e1f7f83f2f3ee073348a1c9955ff046e5e55
rtgamingwdt <46906497+rtgamingwdt@users.noreply.github.com>
2020-12-03 15:40:00 -0500

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -5,15 +5,14 @@ const lyricsFinder = require("lyrics-finder");
 const sendError = require("../util/error");
 
 module.exports.run = async (client, message, args) => {
-    let WARNING = client.emojis.cache.find(emoji => emoji.id === '778868936874655774')
         const queue = message.client.queue.get(message.guild.id);
-        if (!queue) return sendError(`${WARNING}There is nothing playing.`, message.channel).catch(console.error);
+        if (!queue) return sendError(`There is nothing playing.`, message.channel).catch(console.error);
 
         let lyrics = null;
 
         try {
             lyrics = await lyricsFinder(queue.songs[0].title, "");
-            if (!lyrics) lyrics = `${WARNING}No lyrics found for ${queue.songs[0].title}.`;
+            if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
         } catch (error) {
             lyrics = `No lyrics found for ${queue.songs[0].title}.`;
         }
