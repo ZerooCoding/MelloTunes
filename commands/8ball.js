f390632820dab15ba56093c0cb5090dbf400f282
rtgamingwdt <46906497+rtgamingwdt@users.noreply.github.com>
2020-12-03 15:26:37 -0500

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,13 +3,12 @@ const {
 } = require("discord.js");
 const sendError = require("../util/error")
 
-
 module.exports.run = async (client, message, args) => {
  if(!args[0]) {
             sendError('Please ask me a question.', message.channel);
           } else {
               let eightball = require('8ball')()
-              message.reply(`🎱${eightball}`)
+              message.reply(`${eightball}`)
           }
    		
     }
