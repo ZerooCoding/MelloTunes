52566c06e59cecb64656f51312c5fda81cc78979
rtgamingwdt <46906497+rtgamingwdt@users.noreply.github.com>
2020-12-03 15:05:47 -0500

    Create error.js

---
+++ util/error.js
@@ -0,0 +1,11 @@
+const {
+    MessageEmbed
+} = require("discord.js")
+
+module.exports = async (text, channel) => {
+    let embed = new MessageEmbed()
+        .setColor("RED")
+        .setDescription(text)
+        .setFooter("Oops something went wrong :(")
+    await channel.send(embed)
+}
