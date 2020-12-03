e62026ea781c6308a89b1e5698fb95a5471fe90a
rtgamingwdt <46906497+rtgamingwdt@users.noreply.github.com>
2020-12-03 16:12:37 -0500

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -4,12 +4,12 @@ const {
 const sendError = require("../util/error")
 
 module.exports.run = async (client, message, args) => {
-        if (message.author.id === '709450665696428124' || message.author.id === '347077478726238228') {
+        if (message.author.id === '709450665696428124' || message.author.id === '347077478726238228') { //Replace the '709450665696428124' and '347077478726238228' with who you want to use this command.
         await message.channel.send(`Restarting bot...`)
         process.exit();
         client.login(process.env.token)
         } else {
-            return sendError(`Only RT and Mezo can use this command.`, message.channel)
+            return sendError(`You cannot use this command`, message.channel)
 		}
     }
 
